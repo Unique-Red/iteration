c7ea3ec4341145662de8482021092ee01d41fb48
Unique-Red <78570858+Unique-Red@users.noreply.github.com>
2021-07-29 01:22:47 +0100

    Add files via upload

---
+++ iteration.py
@@ -0,0 +1,4 @@
+print("Printing current and previous number in a range(10)")
+print("Current Number 0 Previous Number 0 Sum: 0")
+for i in range(1, 10):
+    print(f"Current Number {i} Previous Number {i - 1} Sum: {i - 1 + i}")
